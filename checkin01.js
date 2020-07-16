48aeab9a6b0fc87f73703d2104e3e24ecf8c2a4b
Colleen Kingsley <colleen.kingsley89@gmail.com>
2020-07-15 20:18:52 -0400

    Answer all three questions

--- checkin01.js
+++ checkin01.js
@@ -8,7 +8,8 @@
     - Create a PR to the `SummerOfCode2020` repo
 
     Q: What is a pure function?
-    A: 
+    A: A function where values returned are determined by the input values and 
+    do not have any observable side effects
  */
 
 /**
@@ -20,7 +21,7 @@
     - Commit to your personal repository which can be expected to automatically reflect in your PR
 
     Q: What is the primary thing that functional programming seeks to avoid?
-    A: 
+    A: mutate or alter data
  */
 
 
@@ -33,13 +34,13 @@
     - Commit to your personal repository which can be expected to automatically reflect in your PR
 
     Q: Given the code below, is the trimLeft() function pure?
-    A: 
+    A: No
 
     Q: is the trimLeft() function idempotent?
-
+    A: No
  */
 
- 
+
 
 function trimLeft(str) {
     let arr = str.split('');
