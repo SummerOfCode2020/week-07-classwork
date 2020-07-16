fb2144476d54c7702f8963008c67975b5182e7ca
Yor Name <hollisjamison@gmail.com>
2020-07-15 20:10:59 -0400

    Answers question 1

--- checkin01.js
+++ checkin01.js
@@ -8,8 +8,10 @@
     - Create a PR to the `SummerOfCode2020` repo
 
     Q: What is a pure function?
-    A: 
+    A:  a pure function is a function that doesn't change any data in the input. You can run it over and over on the same data set and always get the same answer
  */
+   
+
 
 /**
 
