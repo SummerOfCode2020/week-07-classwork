340ca215594bb9264473943d31a39de4971e455d
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-15 21:35:38 -0400

    Answer Checkin02 Questions

--- slide11_isOdd_es6_using_arrays.js
+++ slide11_isOdd_es6_using_arrays.js
@@ -3,36 +3,29 @@
     Now let's remove ourFilter() from the process and use JavaScript's built-in filter(). 
     The results are exactly the same with less code.
 
+    The filter takes a predicate function (return true or false) in order to work
+    If the item returns true - filter will keep item; if the item returns false - filter removes item
+
  */
 function isOdd(n) {
   return n % 2 === 1
 }
 
-// no longer needed:
-function ourFilter(arr, func) {
- let newArr = []
-
-  for (let i = 0; i < arr.length; i++) {
-    if(func(arr[i])) {
-      newArr.push(arr[i)
-    }
-  }
-
-  return newArr
-}
+// no longer needed: removed ourFilter function
 
 const arr = [1, 2, 3, 4, 5]
 const retVal = arr.filter(isOdd)
 
-console.log(retVal)
-console.log(arr)
+console.log({ retVal })
+console.log({ arr })
 
-/*
-slide12 - We also see that calling filter() on an array with only even numbers will return an empty list as we expected.
 
-const arr = [2, 4, 6, 8]
-const retVal = arr.filter(isOdd)
-
-console.log(retVal)
-console.log(arr)
+/* slide12 - We also see that calling filter() on an array with only even numbers will return an
+empty list as we expected.
 */
+
+const arr2 = [2, 4, 6, 8]
+const retVal2 = arr2.filter(isOdd)
+console.log("Expect an empty array result")
+console.log({ retVal2 })
+console.log({ arr2 })
