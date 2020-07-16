07209fc90ac767c70f6b3df17b53fc0b46e142cd
Sharon Lin <hsiang.lin@gmail.com>
2020-07-15 20:01:32 -0400

    Add additional comments notes in slide 4

--- slide04.js
+++ slide04.js
@@ -1,6 +1,8 @@
 /**
 
-    This version of the sumAll() function is neither pure nor idempotent. The reason for this is that it mutates the original array in the process of calculating its total. This side effect makes it so that we can't keep calling sumAll() on the array and receive the same number back.
+    This version of the sumAll() function is neither pure nor idempotent. The reason for this is that it mutates the original 
+    array in the process of calculating its total. This side effect makes it so that we can't keep calling sumAll() 
+    on the array and receive the same number back.
 
     Learn something new!
 
@@ -20,10 +22,16 @@
  */
 function sumAll(ages) {
   let total = 0
-
+  // initialize with a number to prevent errors
+  // if length is 0 then it's false
+  // any number that is not 0 is true in a predicate
+  // ages.length !== 0 (strictly not equal)
+  // === and !== also check for type
   while(ages.length) {
+    
     // `ages` array is being modified
     total += ages.pop()
+    //total = total + ages.pop()
   }
 
   return total
@@ -32,8 +40,14 @@ function sumAll(ages) {
 // given this data
 const dayCampers = [1, 2, 3, 4, 5]
 
+console.log({dayCampers})
+// {} makes it an object
+// expect to see this array [1, 2, 3, 4, 5]
+
 // looping here for example purposes to show that the result is not the same the second and third call
 // We see that this implementation has "side effects" so it is not an "Idempotent" function
 for(let i = 0; i < 3; i++) {
   console.log(sumAll(dayCampers))
 }
+//expect an empty array
+console.log({dayCampers})
